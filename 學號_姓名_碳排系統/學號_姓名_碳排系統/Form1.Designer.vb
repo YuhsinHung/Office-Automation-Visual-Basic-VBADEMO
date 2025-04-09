792dbf48147656c6e654833519e22f6d8ceddb6d
YuhsinHung <31251213+YuhsinHung@users.noreply.github.com>
2025-04-09 11:17:02 +0800

    Add files via upload
    初版檔案上傳

---
+++ 學號_姓名_碳排系統/學號_姓名_碳排系統/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CalForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblNumDevices = New System.Windows.Forms.Label()
+        Me.lblHours = New System.Windows.Forms.Label()
+        Me.lblCO2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txbHours = New System.Windows.Forms.TextBox()
+        Me.txbCO2 = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblTotalDevice = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(325, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(342, 50)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "雲科大碳設備計算"
+        '
+        'lblNumDevices
+        '
+        Me.lblNumDevices.AutoSize = True
+        Me.lblNumDevices.BackColor = System.Drawing.Color.Transparent
+        Me.lblNumDevices.Font = New System.Drawing.Font("微軟正黑體", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblNumDevices.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblNumDevices.Location = New System.Drawing.Point(104, 456)
+        Me.lblNumDevices.Name = "lblNumDevices"
+        Me.lblNumDevices.Size = New System.Drawing.Size(145, 40)
+        Me.lblNumDevices.TabIndex = 1
+        Me.lblNumDevices.Text = "設備數量"
+        '
+        'lblHours
+        '
+        Me.lblHours.AutoSize = True
+        Me.lblHours.BackColor = System.Drawing.Color.Transparent
+        Me.lblHours.Font = New System.Drawing.Font("微軟正黑體", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblHours.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblHours.Location = New System.Drawing.Point(216, 186)
+        Me.lblHours.Name = "lblHours"
+        Me.lblHours.Size = New System.Drawing.Size(127, 36)
+        Me.lblHours.TabIndex = 2
+        Me.lblHours.Text = "使用時間"
+        '
+        'lblCO2
+        '
+        Me.lblCO2.AutoSize = True
+        Me.lblCO2.BackColor = System.Drawing.Color.Transparent
+        Me.lblCO2.Font = New System.Drawing.Font("微軟正黑體", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblCO2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblCO2.Location = New System.Drawing.Point(216, 247)
+        Me.lblCO2.Name = "lblCO2"
+        Me.lblCO2.Size = New System.Drawing.Size(211, 36)
+        Me.lblCO2.TabIndex = 3
+        Me.lblCO2.Text = "每小時碳排放量"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.BackColor = System.Drawing.Color.Transparent
+        Me.lblResult.Font = New System.Drawing.Font("微軟正黑體", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.Red
+        Me.lblResult.Location = New System.Drawing.Point(102, 690)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(262, 50)
+        Me.lblResult.TabIndex = 4
+        Me.lblResult.Text = "分析結果輸出"
+        '
+        'txbHours
+        '
+        Me.txbHours.BackColor = System.Drawing.SystemColors.Info
+        Me.txbHours.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txbHours.Location = New System.Drawing.Point(482, 186)
+        Me.txbHours.Name = "txbHours"
+        Me.txbHours.Size = New System.Drawing.Size(348, 46)
+        Me.txbHours.TabIndex = 6
+        '
+        'txbCO2
+        '
+        Me.txbCO2.BackColor = System.Drawing.SystemColors.Info
+        Me.txbCO2.Font = New System.Drawing.Font("新細明體", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txbCO2.Location = New System.Drawing.Point(482, 247)
+        Me.txbCO2.Name = "txbCO2"
+        Me.txbCO2.Size = New System.Drawing.Size(348, 46)
+        Me.txbCO2.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(183, 345)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(244, 51)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "設備添加"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(561, 345)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(244, 51)
+        Me.btnCalculate.TabIndex = 9
+        Me.btnCalculate.Text = "總碳排放量"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblTotalDevice
+        '
+        Me.lblTotalDevice.AutoSize = True
+        Me.lblTotalDevice.BackColor = System.Drawing.Color.Transparent
+        Me.lblTotalDevice.Font = New System.Drawing.Font("微軟正黑體", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblTotalDevice.ForeColor = System.Drawing.Color.Black
+        Me.lblTotalDevice.Location = New System.Drawing.Point(104, 512)
+        Me.lblTotalDevice.Name = "lblTotalDevice"
+        Me.lblTotalDevice.Size = New System.Drawing.Size(127, 36)
+        Me.lblTotalDevice.TabIndex = 10
+        Me.lblTotalDevice.Text = "設備介紹"
+        '
+        'CalForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1083, 987)
+        Me.Controls.Add(Me.lblTotalDevice)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txbCO2)
+        Me.Controls.Add(Me.txbHours)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblCO2)
+        Me.Controls.Add(Me.lblHours)
+        Me.Controls.Add(Me.lblNumDevices)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "CalForm"
+        Me.Text = "設備碳排監控系統"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblNumDevices As Label
+    Friend WithEvents lblHours As Label
+    Friend WithEvents lblCO2 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txbHours As TextBox
+    Friend WithEvents txbCO2 As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblTotalDevice As Label
+End Class
---
+++ 學號_姓名_碳排系統/學號_姓名_碳排系統/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class CalForm
+
+    '宣告變數-和JAVA依樣
+    ' 初始化總二氧化碳排放量變數
+    Dim totalCO2 As Double = 0.0
+    ' 設定設備數量的變數
+    Dim numDevices As Integer
+    ' 設備使用小時數的陣列
+    Dim deviceHours() As Double
+    ' 每小時二氧化碳排放量的陣列
+    Dim co2PerHour() As Double
+
+
+
+    Private Sub CalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' 初始化設備使用小時數的陣列,設為0
+        ReDim deviceHours(0)
+        '初始化每小時二氧化碳排放量的陣列,設為0
+        ReDim co2PerHour(0)
+        '[學生改]請把雲寶寶改成你名字,並於聯絡方式補上你的EMAIL
+        MsgBox("歡迎使用碳排放計算系統！藉由輸入設備數量、使用時間及每小時碳排放量。本系統將提供計算結果以供參考" & vbCrLf & "任何問題請找窗口雲寶寶,聯絡方式", MsgBoxStyle.Information, "初始化")
+        '進階語法:未來可以加入vbCrLf來換行,範例MsgBox("這是第一行" & vbCrLf & "這是第二行")
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' 增加設備數量,陣列總容量更新
+        numDevices += 1
+        ' 設備使用時間陣列容量為最新容量
+        '[公式] ReDim Preserve 陣列名稱(容量)
+        ReDim Preserve deviceHours(numDevices)
+        ' 使用小時數陣列增加1為最新容量
+        '[公式] ReDim Preserve 陣列名稱(容量)
+        ReDim Preserve co2PerHour(numDevices)
+        '宣告lastMachineIdx為陣列索引
+        Dim lastMachineIdx As Integer
+        '最後一個陣列元素索引=最新容量
+        lastMachineIdx = numDevices
+        '初始化-重新更新設備計算結果
+        lblNumDevices.Text = "目前設備共" & numDevices.ToString() & "台"
+
+        ' 輸入框設備使用時間加入陣列
+        deviceHours(lastMachineIdx) = CDbl(txbHours.Text)
+        '輸入框每小時碳排放量加入陣列
+        co2PerHour(lastMachineIdx) = CDbl(txbCO2.Text)
+
+        ' 清空輸入框
+        txbHours.Clear()
+        txbCO2.Clear()
+        Dim deviceNote As String
+        deviceNote = "設備" & numDevices.ToString() & ",使用時間" & deviceHours(lastMachineIdx) & "小時;單位碳排量=" & co2PerHour(lastMachineIdx)
+
+        '每台設備,用累加APPEND方式顯示於介面
+        lblTotalDevice.Text += vbCrLf & deviceNote
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' 總二氧化碳排放量
+        totalCO2 = 0.0
+        '宣告設備索引
+        Dim mIdx As Integer
+        For mIdx = 1 To numDevices
+            Dim deviceCO2 As Double
+
+            ' 如果每小時碳排放量超過零，進行計算
+            If co2PerHour(mIdx) > 0 Then
+                deviceCO2 = deviceHours(mIdx) * co2PerHour(mIdx)
+                '否則設為0
+            Else
+                deviceCO2 = 0
+            End If
+
+            totalCO2 += deviceCO2
+        Next
+        '輸出結果
+        lblResult.Text = "總碳排放量（公斤）： " & totalCO2
+
+    End Sub
+End Class
